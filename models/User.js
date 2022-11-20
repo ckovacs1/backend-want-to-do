8abf51e8bfff935ca7a685a51b4bf1fd4cf5d74d
les5185 <70672721+les5185@users.noreply.github.com>
2022-11-20 09:01:04 -0500

    Add user model gitignore and prettier

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//update userschema after confirming which info should be under user
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -5,31 +5,28 @@ const app = express();
 
 app.use(cors());
 
-app.get('/profile', (req,res) => {
-    res.json(
-        {
-            "profiles": [
-                {
-                    name: 'Charles Leclerc',
-                    email: 'cleclerc@gmail.com',
-                    bio: 'Charles Perceval Leclerc is a Monégasque racing driver, currently racing in Formula One for Scuderia Ferrari. He won the GP3 Series championship in 2016 and the FIA Formula 2 Championship in 2017.'
-                },
-                {
-                    name: 'Lewis Hamilton',
-                    email: 'lhamilton@mercedes.com',
-                    bio: 'In Formula One, Hamilton has won a joint-record seven World Drivers Championship titles, and holds the records for the most wins, pole positions, and podium finishes, among others'
-                },
-                {
-                    name: 'Nicolas Latifi',
-                    email: 'test',
-                    bio: 'test'
-                }
-            ]
-        }
-       
-    );
-})
+app.get('/profile', (req, res) => {
+  res.json({
+    profiles: [
+      {
+        name: 'Charles Leclerc',
+        email: 'cleclerc@gmail.com',
+        bio: 'Charles Perceval Leclerc is a Monégasque racing driver, currently racing in Formula One for Scuderia Ferrari. He won the GP3 Series championship in 2016 and the FIA Formula 2 Championship in 2017.',
+      },
+      {
+        name: 'Lewis Hamilton',
+        email: 'lhamilton@mercedes.com',
+        bio: 'In Formula One, Hamilton has won a joint-record seven World Drivers Championship titles, and holds the records for the most wins, pole positions, and podium finishes, among others',
+      },
+      {
+        name: 'Nicolas Latifi',
+        email: 'test',
+        bio: 'test',
+      },
+    ],
+  });
+});
 
 app.listen(8080, () => {
-    console.log('Server on port 8080')
-})
+  console.log('Server on port 8080');
+});
