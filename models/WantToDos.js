1040b941930754eb25ffcae4434ed8ea6bf50f03
Jennifer Tang <jt3744@nyu.edu>
2022-12-03 01:23:27 -0500

    edit to-do schema

--- models/WantToDos.js
+++ models/WantToDos.js
@@ -7,9 +7,9 @@ const WantToDoSchema = new mongoose.Schema({
     // _id by default
     title: {type: String, default: "Untitled Want-To-Do"},
     description: String, // assume it's not required 
-    startDateTime: {type: String, required: true},
-    endDateTime: {type: String, required: true}, // will making sure this date is later than startDate be handled before creating new doc? 
-    repetition: {type: String, required: true}, // looks like repition value is a string
+    startDateTime: {type: Date, default: Date.now},
+    endDateTime: {type: Date, default: Date.now}, // will making sure this date is later than startDate be handled before creating new doc? 
+    repetition: {type: Boolean}, // looks like repition value is a string
     repeatOn: [String],
     category: {type: String, required: true},
     //inviteFriends: [{type: mongoose.Types.ObjectId, ref: 'User'}]
