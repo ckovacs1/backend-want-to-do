08926cc880ebeab452aad2eb0ac45b60ad458632
les5185 <70672721+les5185@users.noreply.github.com>
2022-12-09 02:36:36 -0500

    Add add wanttodo for both users if friends

--- server.js
+++ server.js
@@ -217,9 +217,12 @@ app.get(
       return res.status(200).json({ success: false, following: [] });
     }
 
-    User.findOne({ _id: req.user.id }).exec(function (err, user) {
+    User.find({ _id: { $in: req.user.following } }).exec(function (
+      err,
+      following,
+    ) {
       if (err) return res.status(400).json({ success: false, error: err });
-      return res.status(200).json({ success: true, following: user.following });
+      return res.status(200).json({ success: true, following });
     });
   },
 );
